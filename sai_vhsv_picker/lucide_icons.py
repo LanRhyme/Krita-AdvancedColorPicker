7a1e2f974d45a6624d42a557a0601c77dfc78300
LanRhyme <xiao_ren233@foxmail.com>
2026-07-30 22:57:44 +0800

    fix: 修复 lucide_icons 中导入 qt_compat 兼容枚举 Symbol 缺失问题

--- sai_vhsv_picker/lucide_icons.py
+++ sai_vhsv_picker/lucide_icons.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Lucide Icons renderer for sai_vhsv_picker"""
 
-from .qt_compat import QIcon, QPixmap, QPainter, QColor, QByteArray, QSvgRenderer
+from .qt_compat import *
 
 _LUCIDE_SVGS = {
     "shield": """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/></svg>""",
